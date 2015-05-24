5e2206516f1f350c25fda65bdd57d9e308ac51d2
Ali Burak ÖNCÜL <boncul@bil.omu.edu.tr>
2015-05-24 19:47:46 +0300

    c# sort mükemmel

---
+++ karalama-defteri/c#/csharp_karakter_islemleri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program {
+	static void Main() {
+		string[] kelimeler = new string[] {
+			"ALİ",
+			"veli",
+			"özkan",
+			"İsmet",
+			"ÖZTÜRK",
+			"Ümit",
+			"ilteriş",
+			"Mahmut",
+			"Dağcan",
+			"şevket",
+			"Şevket"
+		};
+
+		foreach (string s in kelimeler) {
+			Console.WriteLine (s.ToLower ());
+		}
+
+		Console.WriteLine ("---");
+
+		foreach (string s in kelimeler) {
+			Console.WriteLine (s.ToUpper ());
+		}
+	}
+}
