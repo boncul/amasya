4b37f28e71a5c5ea43434ecb6f89506e50451662
Ali Burak ÖNCÜL <boncul@bil.omu.edu.tr>
2015-05-24 19:35:26 +0300

    c# sort hatalı

---
+++ karalama-defteri/c#/csharp_sort.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+	static void Main() {
+		char[] kelimeler = {'i', 'ö', 'İ', 'Ü', 'ş', 'M', 'ğ', 'Ş'};
+		Array.Sort<char>(kelimeler);
+		foreach (var c in kelimeler)
+			Console.WriteLine(c);
+	}
+}
