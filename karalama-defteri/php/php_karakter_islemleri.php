98788624146f553ea387610ce5dc319ef7c33924
Ali Burak ÖNCÜL <boncul@bil.omu.edu.tr>
2015-05-27 17:46:02 +0300

    PHP'de karakter işlemleri hatalı

---
+++ karalama-defteri/php/php_karakter_islemleri.php
@@ -0,0 +1,12 @@
+<?php
+
+$kelimeler = " İİII ALİ veli özkan İsmet ÖZTÜRK ümit ilteriş Mahmut Dağcan şevket Şevket";
+
+$kelimeler_b = strtoupper($kelimeler);
+$kelimeler_k = strtolower($kelimeler);
+
+echo $kelimeler_b;
+echo "---";
+echo $kelimeler_k;
+
+?>
